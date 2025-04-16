62b81a6f343f3b885daacdf6836d1b027d917b46
Parsa Dormiani <pdormiani@gmail.com>
2025-04-15 23:40:39 -0700

    Corrections to oil configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -781,12 +781,12 @@ require("oil").setup({
     -- Can be "fast", true, or false. "fast" will turn it off for large directories.
     natural_order = "fast",
     -- Sort file and directory names case insensitive
-    case_insensitive = false,
+    case_insensitive = true,
     sort = {
       -- sort order can be "asc" or "desc"
       -- see :help oil-columns to see which columns are sortable
-      { "type", "asc" },
       { "name", "asc" },
+      { "type", "asc" },
     },
     -- Customize the highlight group for the file name
     highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
@@ -799,13 +799,13 @@ require("oil").setup({
   git = {
     -- Return true to automatically git add/mv/rm files
     add = function(path)
-      return false
+      return true
     end,
     mv = function(src_path, dest_path)
-      return false
+      return true
     end,
     rm = function(path)
-      return false
+      return true
     end,
   },
   -- Configuration for the floating window in oil.open_float
@@ -822,7 +822,7 @@ require("oil").setup({
     -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
     get_win_title = nil,
     -- preview_split: Split direction: "auto", "left", "right", "above", "below".
-    preview_split = "auto",
+    preview_split = "below",
     -- This is the config that will be passed to nvim_open_win.
     -- Change values here to customize the layout
     override = function(conf)
